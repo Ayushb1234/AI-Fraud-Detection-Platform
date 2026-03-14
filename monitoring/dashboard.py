2eaf6031ee3364032543f0dea2e811c9f754344c
Ayush Choudhary <ka843004@gmail.com>
2026-03-14 13:35:44 +0530

    dashboard Implementation

---
+++ monitoring/dashboard.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import random
+import time
+
+st.set_page_config(page_title="Fraud Monitoring Dashboard", layout="wide")
+
+st.title("💳 Real-Time Fraud Detection Dashboard")
+
+placeholder = st.empty()
+
+data = []
+
+while True:
+
+    transaction = {
+        "amount": random.uniform(10,2000),
+        "fraud_probability": random.uniform(0,1)
+    }
+
+    transaction["decision"] = "BLOCK" if transaction["fraud_probability"] > 0.8 else "ALLOW"
+
+    data.append(transaction)
+
+    df = pd.DataFrame(data)
+
+    with placeholder.container():
+
+        col1, col2, col3 = st.columns(3)
+
+        col1.metric("Total Transactions", len(df))
+        col2.metric("Frauds Detected", len(df[df["decision"]=="BLOCK"]))
+        col3.metric("Fraud Rate", f"{(len(df[df['decision']=='BLOCK'])/len(df))*100:.2f}%")
+
+        st.line_chart(df["fraud_probability"])
+
+        st.dataframe(df.tail(10))
+
+    time.sleep(2)
